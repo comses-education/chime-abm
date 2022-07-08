cef595993f427ecd62fa17609138f3e19e147658
Allen Lee <allen.lee@asu.edu>
2022-07-07 23:18:37 -0700

    remove JAVA_HOME again
    that path only works in the comses/netlogo image, not the
    comses/osg-netlogo image (for now)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM comses/osg-netlogo:6.2.2
 
-ENV JAVA_HOME=/usr/local/openjdk-11
-
 LABEL maintainer="CoMSES Net <support@comses.net>"
 
 WORKDIR /code
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,3 @@
-export JAVA_HOME=/usr/local/openjdk-11
-
 /opt/netlogo/netlogo-headless.sh \
     --model /code/CHIME.nlogo \
     --experiment osg_experiment_nocensus_factors \
