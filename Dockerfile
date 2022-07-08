a8be0b87b6f63cf50ee3f8f72c5c5128523552a4
Allen Lee <allen.lee@asu.edu>
2022-07-07 17:30:49 -0700

    fix: set base JAVA_HOME in environment

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM comses/osg-netlogo:6.2.2
 
+ENV JAVA_HOME=/usr/local/openjdk-11
+
 LABEL maintainer="CoMSES Net <support@comses.net>"
 
 WORKDIR /code
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
+export JAVA_HOME=/usr/local/openjdk-11
+
 /opt/netlogo/netlogo-headless.sh \
     --model /code/CHIME.nlogo \
     --experiment osg_experiment_nocensus_factors \
